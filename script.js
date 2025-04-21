e6ec1e1e09443f24376432cc0faa2da254cd9113
Karankk85 <karkk8533@gmail.com>
2025-04-21 19:26:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach(sound => {
+  const audio = document.createElement('audio');
+  audio.src = `sounds/${sound}.mp3`;
+  audio.id = sound;
+  document.body.appendChild(audio);
+});
+
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const sound = button.innerText.toLowerCase();
+
+    stopSounds();
+
+    if (sound !== 'stop') {
+      document.getElementById(sound).play();
+    }
+  });
+});
+
+function stopSounds() {
+  sounds.forEach(sound => {
+    const audio = document.getElementById(sound);
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
